ccf810929eaa304ad43b3400276751d99d1a6d2d
Clem-prog <clementisjs747@gmail.com>
2024-12-22 21:21:15 +0700

    modified routers and added middleware for user.

--- src/routers/protected-router.ts
+++ src/routers/protected-router.ts
@@ -1,3 +1,8 @@
 import express from "express";
+import { userMiddleware } from "../middleware/user-middleware";
+import { UserController } from "../controllers/userController";
 
 export const protectedRouter = express.Router()
+protectedRouter.use(userMiddleware)
+
+protectedRouter.delete("/api/logout", UserController.logout)
